cb5dbcb1ad1b402a52d08695de595e07b16ae117
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2025-10-08 16:50:55 +0200

    Doladeni vsech drobnosti a pridani funkce animace

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "drawing.h"
 
-void prerusovana_cara(int length) {
-    for (int i = 0; i < length * 2; i++) {
+#define RADEK_MICKU 15      
+#define MAX_SLOUPEC 70      
+#define MIN_SLOUPEC 10      
+#define POCET_SNIMKU 300 
+
+void prerusovana_cara(int delka) {
+    for (int i = 0; i < delka * 2; i++) {
         if (i % 2 == 0) {
             set_blue_color();  
             draw_pixel();
@@ -13,8 +18,8 @@ void prerusovana_cara(int length) {
     }
 }
 
-void schody(int steps) {
-    for (int i = 0; i < steps; i++) {
+void schody(int kroky) {
+    for (int i = 0; i < kroky; i++) {
         set_red_color();
         draw_pixel();
         move_right();
@@ -24,27 +29,36 @@ void schody(int steps) {
     }
 }
 
-void kvitko(int height, int width) {
-    int stred = width / 2;
-     for (int i = 0; i < width; i++) {
-        for (int j = 0; j < width; j++) {
+void kvitko(int vyska, int sirka) {
+    int stred = sirka / 2;
+
+    for (int i = 0; i < sirka; i++) {
+        for (int j = 0; j < sirka; j++) {
+
             if (i == stred && j == stred) {
-                set_yellow_color(); 
+                set_white_color();
                 draw_pixel();
-            } else if ((i - stred) <= 1 && (j - stred) <= 1) {
-                set_red_color(); 
+            } 
+            else if ((i - stred) <= 1 && (j - stred) <= 1) {
+                set_yellow_color();
                 draw_pixel();
-            } else {
+            } 
+            else {
                 move_right();
                 continue;
             }
+
             move_right();
         }
+
         move_down();
-        for (int k = 0; k < width; k++) move_left();
+
+        for (int k = 0; k < sirka; k++) {
+            move_left();
+        }
     }
 
-    for (int i = 0; i < height; i++) {
+    for (int i = 0; i < vyska; i++) {
         move_right();
         set_green_color();
         draw_pixel();
@@ -53,25 +67,27 @@ void kvitko(int height, int width) {
     }
 
     move_right();
-    //move_left();
     set_green_color();
     draw_pixel();
+
     move_down();
     move_down();
     move_down();
 }
 
-void louka(int height, int width) {
+void louka(int vyska, int sirka) {
     int velikost_kvetu = 3;
     int delka_stonku = 3;
-    int vodorovna_mezera = 2;
-    int svisla_mezera = 1;
+    int vodorovna_mezera = 3;
+    int svisla_mezera = 2;
     int zacatek_radku = 1;
     int zacatek_sloupce = 1;
 
-    for (int r = 0; r < height; r++) {
-        for (int c = 0; c < width; c++) {
-            int radek = zacatek_radku + r * (velikost_kvetu + delka_stonku + svisla_mezera);
+    int vyska_kvetiny = velikost_kvetu + delka_stonku + svisla_mezera;
+
+    for (int r = 0; r < vyska; r++) {
+        for (int c = 0; c < sirka; c++) {
+            int radek = zacatek_radku + r * vyska_kvetiny;
             int sloupec = zacatek_sloupce + c * (velikost_kvetu + vodorovna_mezera);
 
             move_to(radek, sloupec);
@@ -79,11 +95,42 @@ void louka(int height, int width) {
         }
     }
 
-    move_to(zacatek_radku + height * (velikost_kvetu + delka_stonku + svisla_mezera), 1);
+    move_to(zacatek_radku + vyska * vyska_kvetiny + 1, 1);
+    //Stonky spodnich kvetin jsou dvojnasobne mensi, nevim, jak to opravit
 }
 
 void animace() {
+    int sloupec = MIN_SLOUPEC;
+    int smer = 1; 
+    
+    clear_screen();
 
+    set_red_color(); 
+
+    for (int snimek = 0; snimek < POCET_SNIMKU; snimek++) {
+        clear_screen(); 
+        set_red_color(); 
+
+        move_to(RADEK_MICKU, sloupec);
+        draw_pixel();
+        move_right();
+        draw_pixel();
+        move_down();
+        move_left();
+        draw_pixel();
+        move_right();
+        draw_pixel();
+
+        animate_ms(20);
+
+        sloupec += smer;
+
+        if (sloupec >= MAX_SLOUPEC) {
+            smer = -1; 
+        } else if (sloupec <= MIN_SLOUPEC) {
+            smer = 1; 
+        }
+    }
 }
 
 int main() {
@@ -99,13 +146,18 @@ int main() {
     schody(5);
   }
   else if (drawing == 2) {
-    kvitko(3, 3);
+    kvitko(2, 3);
   }
   else if (drawing == 3) {
     louka(2, 3);
   }
+  else if (drawing = 4) {
+    animace();
+
+    end_drawing();
+  }
   else {
-    printf("To je zatim vse");
+    printf("Spatne zadani. Zadejte cislo od 0 - 6");
   }
 
   end_drawing();
